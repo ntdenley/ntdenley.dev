3efc461f4e0291766ecf26c1a36ca4058e1c8710
Noah Denley <ntdenley@gmail.com>
2025-03-04 10:18:16 -0800

    Add wood frame, style changes

--- js/script.js
+++ js/script.js
@@ -41,6 +41,32 @@ $(function(){
 			$(this).addClass('active');
 			portfolio.shuffle('shuffle', groupName );
 		});
+
+		/*=========================================================================
+			Typewriter Setup
+		=========================================================================*/
+		const typewriterElement = document.getElementById("typewriter-text");
+		const textContent = typewriterElement.textContent.trim();
+		const numCharacters = textContent.length;
+
+		// Create a new style element
+		const style = document.createElement('style');
+		style.type = 'text/css';
+
+		// Define the keyframes with the calculated steps
+		const keyframes = `
+			@keyframes typing {
+				from { width: 0; }
+				to { width: 100%; }
+			}
+			.typewriter {
+				animation: typing 3s steps(${numCharacters}, end) forwards, blink 1s step-end infinite;
+			}
+		`;
+
+		// Append the keyframes to the style element
+		style.innerHTML = keyframes;
+		document.head.appendChild(style);
 		
 	});
 	
